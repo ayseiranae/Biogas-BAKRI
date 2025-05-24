c4205e9a4e3eee4ecda60d84c6eb2ea277caff0d
Ayseira Hidane <riesyanadiha@gmail.com>
2025-05-24 21:29:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ function explore() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const joinButtons = document.querySelectorAll(".join-btn");
-  joinButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const topicTitle =
-        this.parentElement.querySelector(".topic-title").textContent;
-      alert(`You are joining the discussion: ${topicTitle}`);
-    });
-  });
 
   const editProfileBtn = document.querySelector(".edit-profile-btn");
   editProfileBtn.addEventListener("click", function () {
